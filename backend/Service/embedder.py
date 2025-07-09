33bb5254da16a575f0fb8387a32be40cfe52fe2b
Nguyễn Hoàng Thắng <shea11082003@gmail.com>
2025-07-09 08:55:20 +0700

    Delete old files

--- backend/Service/embedder.py
+++
@@ -1,8 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-class Embedding:
-    def __init__(self, model_name='all-MiniLM-L6-v2'):
-        self.model = SentenceTransformer(model_name);
-    
-    def embed(self, text):
-        return self.model.encode(text, convert_to_numpy=True)
